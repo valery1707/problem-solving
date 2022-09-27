51f877db59165a3d543446add5177a21c8205d6e
Valeriy Vyrva <valery1707@gmail.com>
2022-09-27 11:25:50 +0300

    Use AssertJ library in tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 
 dependencies {
   testImplementation(kotlin("test"))
+  testImplementation("org.assertj:assertj-core:3.22.0")
 }
 
 //Encoding
---
+++ src/test/kotlin/name/valery1707/problem/SimpleKotlinTests.kt
@@ -0,0 +1,11 @@
+package name.valery1707.problem
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SimpleKotlinTests {
+    @Test
+    internal fun test1() {
+        assertThat("").isBlank
+    }
+}
