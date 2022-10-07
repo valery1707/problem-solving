192751f16bab5542933d676aaf8ee6af0c37f42e
Valeriy Vyrva <valery1707@gmail.com>
2022-10-07 17:26:15 +0300

    EOlymp: Простая задача

---
+++ src/main/kotlin/name/valery1707/problem/e/olymp/Problem1K.kt
@@ -0,0 +1,33 @@
+package name.valery1707.problem.e.olymp
+
+/**
+ * # Простая задача
+ *
+ * Программа считывает двузначное число и выводит через пробел каждую цифру отдельно.
+ *
+ * ## Входные данные
+ *
+ * Натуральное число из промежутка от `10` до `99` включительно.
+ *
+ * ## Выходные данные
+ *
+ * Два одноцифровых числа, разделенных пробелом.
+ */
+interface Problem1K : ProblemConsole {
+    @Suppress("EnumEntryName", "unused")
+    enum class Implementation : Problem1K {
+        scanner {
+            override fun main(args: Array<String>) {
+                val scan = java.util.Scanner(System.`in`)
+                val src = scan.nextInt()
+                println("${src / 10} ${src % 10}")
+            }
+        },
+        read {
+            override fun main(args: Array<String>) {
+                val src = readln().toInt()
+                println("${src / 10} ${src % 10}")
+            }
+        },
+    }
+}
---
+++ src/main/kotlin/name/valery1707/problem/e/olymp/ProblemConsole.kt
@@ -0,0 +1,5 @@
+package name.valery1707.problem.e.olymp
+
+interface ProblemConsole {
+    fun main(args: Array<String>);
+}
